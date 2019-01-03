68db8451c1fc017bd2d0ca58e874c7b3728224b5
tlab i <itlab@itlabui-imac.local>
2019-01-03 15:43:36 +0900

    Initial Commit

---
+++ RecreationManager/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  RecreationManager
+//
+//  Created by itlab on 03/01/2019.
+//  Copyright © 2019 FuLi. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ RecreationManager/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  RecreationManager
+//
+//  Created by itlab on 03/01/2019.
+//  Copyright © 2019 FuLi. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
